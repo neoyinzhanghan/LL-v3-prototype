3dab496d8e99e563ab2cb02e66473ced4110110f
Neo Yin <yinzhang@berkeley.edu>
2024-10-23 02:56:19 -0700

    mass downsampling script

--- downsample_images.py
+++ downsample_images.py
@@ -12,6 +12,8 @@ slide_source_dir = "/pesgisipth/NDPI"
 save_dir_ndpi = "/media/hdd3/neo/error_slides_ndpi"
 save_dir_dzsave = "/media/hdd3/neo/error_slides_dzsave"
 
+already_downsampled_df_oath = "/media/hdd3/neo/error_slides_dzsave/already_downsampled.csv"
+
 pipeline_run_history = pd.read_csv(pipeline_run_history_path)
 
 # get the path to all the jpg file names in selected_topviews
@@ -48,5 +50,42 @@ for name in tqdm(all_names, desc="Getting WSI names"):
 wsi_names_df = pd.DataFrame(wsi_names_df)
 wsi_names_df.to_csv("selected_topviews/wsi_names.csv", index=False)
 
+presaved_df = pd.read_csv(already_downsampled_df_oath)
 
+# get the column wsi_name from presaved_df as a list
+presaved_wsi_names = presaved_df["wsi_name"].tolist()
 
+wsi_names_to_process = [wsi_name for wsi_name in wsi_names if wsi_name not in presaved_wsi_names]
+
+print(f"Founds {len(wsi_names)} WSI tiles.")
+print(f"Found {len(presaved_wsi_names)} WSI tiles already downsampled.")
+print(f"Processing {len(wsi_names_to_process)} WSI tiles...")
+
+already_downsampled_df = {
+    "wsi_name": [],
+    "dzsave_dir": []
+}
+
+try:
+    for wsi_name in tqdm(wsi_names, desc="Saving WSI Tiles"):
+        # remove the .ndpi extension
+        wsi_name_no_ext = wsi_name[:-5]
+
+        # check to see if the wsi_name_no_ext folder already exists in save_dir_dzsave
+        if os.path.exists(os.path.join(save_dir_dzsave, wsi_name_no_ext)):
+            print(f"Folder {wsi_name_no_ext} already exists in {save_dir_dzsave}. Skipping...")
+            continue
+
+        # create the dzsave directory
+        dzsave_dir = os.path.join(save_dir_dzsave, wsi_name_no_ext)
+
+        # downsample the images
+        downsample_slide_dzsave_dir(dzsave_dir)
+
+        already_downsampled_df["wsi_name"].append(wsi_name)
+        already_downsampled_df["dzsave_dir"].append(dzsave_dir)
+
+except Exception as e:
+    already_downsampled_df = pd.DataFrame(already_downsampled_df)
+    already_downsampled_df.to_csv(already_downsampled_df_oath, index=False)
+    raise e
