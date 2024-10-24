3a79907ce60134205704035d617b6d59465f7890
Neo Yin <yinzhang@berkeley.edu>
2024-10-24 11:01:30 -0700

    high mag and low mag tests

--- downsample_images.py
+++ downsample_images.py
@@ -49,70 +49,71 @@ def did_we_downsample(dzsave_dir):
         
     return True
 
-for name in tqdm(all_names, desc="Getting WSI names"):
-    pipeline = name.split("_")[0]
-    datetime_processed = name.split("_")[1].split(".")[0]
+if __name__ == "__main__":
+    for name in tqdm(all_names, desc="Getting WSI names"):
+        pipeline = name.split("_")[0]
+        datetime_processed = name.split("_")[1].split(".")[0]
 
-    # look for the row with pipeline and datetime_processed
-    row = pipeline_run_history[(pipeline_run_history["pipeline"] == pipeline) & (pipeline_run_history["datetime_processed"] == datetime_processed)]
+        # look for the row with pipeline and datetime_processed
+        row = pipeline_run_history[(pipeline_run_history["pipeline"] == pipeline) & (pipeline_run_history["datetime_processed"] == datetime_processed)]
 
-    # assert that exactly one row is found
-    assert len(row) == 1, f"Expected 1 row, got {len(row)} rows for {name}"
+        # assert that exactly one row is found
+        assert len(row) == 1, f"Expected 1 row, got {len(row)} rows for {name}"
 
-    wsi_name = row["wsi_name"].values[0]
+        wsi_name = row["wsi_name"].values[0]
 
-    wsi_names.append(wsi_name)
+        wsi_names.append(wsi_name)
 
-    wsi_names_df["pipeline"].append(pipeline)
-    wsi_names_df["datetime_processed"].append(datetime_processed)
-    wsi_names_df["wsi_name"].append(wsi_name)
+        wsi_names_df["pipeline"].append(pipeline)
+        wsi_names_df["datetime_processed"].append(datetime_processed)
+        wsi_names_df["wsi_name"].append(wsi_name)
 
-wsi_names_df = pd.DataFrame(wsi_names_df)
-wsi_names_df.to_csv("selected_topviews/wsi_names.csv", index=False)
+    wsi_names_df = pd.DataFrame(wsi_names_df)
+    wsi_names_df.to_csv("selected_topviews/wsi_names.csv", index=False)
 
-presaved_df = pd.read_csv(already_downsampled_df_path)
+    presaved_df = pd.read_csv(already_downsampled_df_path)
 
-# get the column wsi_name from presaved_df as a list
-presaved_wsi_names = presaved_df["wsi_name"].tolist()
+    # get the column wsi_name from presaved_df as a list
+    presaved_wsi_names = presaved_df["wsi_name"].tolist()
 
-wsi_names_to_process = [wsi_name for wsi_name in wsi_names if wsi_name not in presaved_wsi_names]
-wsi_names_already_downsampled = [wsi_name for wsi_name in wsi_names if wsi_name in presaved_wsi_names]
+    wsi_names_to_process = [wsi_name for wsi_name in wsi_names if wsi_name not in presaved_wsi_names]
+    wsi_names_already_downsampled = [wsi_name for wsi_name in wsi_names if wsi_name in presaved_wsi_names]
 
-print(f"Founds {len(wsi_names)} WSI tiles.")
-print(f"Found {len(presaved_wsi_names)} WSI tiles already downsampled.")
-print(f"Processing {len(wsi_names_to_process)} WSI tiles...")
+    print(f"Founds {len(wsi_names)} WSI tiles.")
+    print(f"Found {len(presaved_wsi_names)} WSI tiles already downsampled.")
+    print(f"Processing {len(wsi_names_to_process)} WSI tiles...")
 
-already_downsampled_df = {
-    "wsi_name": [],
-    "dzsave_dir": []
-}
+    already_downsampled_df = {
+        "wsi_name": [],
+        "dzsave_dir": []
+    }
 
-for wsi_name in wsi_names_already_downsampled:
-    already_downsampled_df["wsi_name"].append(wsi_name)
-    already_downsampled_df["dzsave_dir"].append(os.path.join(save_dir_dzsave, wsi_name[:-5]))
+    for wsi_name in wsi_names_already_downsampled:
+        already_downsampled_df["wsi_name"].append(wsi_name)
+        already_downsampled_df["dzsave_dir"].append(os.path.join(save_dir_dzsave, wsi_name[:-5]))
 
-for wsi_name in tqdm(wsi_names, desc="Saving WSI Tiles"):
-    # remove the .ndpi extension
-    wsi_name_no_ext = wsi_name[:-5]
+    for wsi_name in tqdm(wsi_names, desc="Saving WSI Tiles"):
+        # remove the .ndpi extension
+        wsi_name_no_ext = wsi_name[:-5]
 
-    # create the dzsave directory
-    dzsave_dir = os.path.join(save_dir_dzsave, wsi_name_no_ext)
+        # create the dzsave directory
+        dzsave_dir = os.path.join(save_dir_dzsave, wsi_name_no_ext)
 
-    # check to see if the wsi_name_no_ext folder already exists in save_dir_dzsave
-    already_done = did_we_downsample(dzsave_dir)
+        # check to see if the wsi_name_no_ext folder already exists in save_dir_dzsave
+        already_done = did_we_downsample(dzsave_dir)
 
-    if already_done:
-        print(f"Folder {wsi_name_no_ext} already fully processed. Skipping...")
+        if already_done:
+            print(f"Folder {wsi_name_no_ext} already fully processed. Skipping...")
+            already_downsampled_df["wsi_name"].append(wsi_name)
+            already_downsampled_df["dzsave_dir"].append(dzsave_dir)
+            already_downsampled_df_to_save = pd.DataFrame(already_downsampled_df)
+            already_downsampled_df_to_save.to_csv(already_downsampled_df_path, index=False)
+            continue
+
+        # downsample the images
+        downsample_slide_dzsave_dir(dzsave_dir)
         already_downsampled_df["wsi_name"].append(wsi_name)
         already_downsampled_df["dzsave_dir"].append(dzsave_dir)
+
         already_downsampled_df_to_save = pd.DataFrame(already_downsampled_df)
         already_downsampled_df_to_save.to_csv(already_downsampled_df_path, index=False)
-        continue
-
-    # downsample the images
-    downsample_slide_dzsave_dir(dzsave_dir)
-    already_downsampled_df["wsi_name"].append(wsi_name)
-    already_downsampled_df["dzsave_dir"].append(dzsave_dir)
-
-    already_downsampled_df_to_save = pd.DataFrame(already_downsampled_df)
-    already_downsampled_df_to_save.to_csv(already_downsampled_df_path, index=False)
