c0d03585aacb8ccc3e32634a73abd1f81a973a8a
Neo Yin <yinzhang@berkeley.edu>
2024-10-23 17:13:43 -0700

    script for running all low mag

--- downsample_images.py
+++ downsample_images.py
@@ -98,10 +98,14 @@ for wsi_name in tqdm(wsi_names, desc="Saving WSI Tiles"):
 
     if already_done:
         print(f"Folder {wsi_name_no_ext} already fully processed. Skipping...")
+        already_downsampled_df["wsi_name"].append(wsi_name)
+        already_downsampled_df["dzsave_dir"].append(dzsave_dir)
         continue
 
     # downsample the images
     downsample_slide_dzsave_dir(dzsave_dir)
-
     already_downsampled_df["wsi_name"].append(wsi_name)
     already_downsampled_df["dzsave_dir"].append(dzsave_dir)
+
+    already_downsampled_df = pd.DataFrame(already_downsampled_df)
+    already_downsampled_df.to_csv(already_downsampled_df_oath, index=False)
